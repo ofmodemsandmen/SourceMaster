b889630a115a1a1d5845d622d7cfd1b4c664fb6a
Dairyman <ralph@rfactory.org>
2023-12-20 07:48:29 -0800

    remove extra packages
    remove extra packages

--- package/rooter-extra/ext-flash/files/usr/lib/flash/setrandom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. /lib/functions.sh
-
-set=$1
-
-uci set flash.flash.random=$set
-uci commit flash
--- package/rooter-extra/ext-ota/files/usr/lib/flash/setenable.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. /lib/functions.sh
-
-set=$1
-
-uci set flash.flash.enabled="$set"
-uci commit flash
