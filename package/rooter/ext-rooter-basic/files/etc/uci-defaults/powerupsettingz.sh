666b2b6c4b3df1f2c04912e861b39bbebb4de2cb
Dairyman <ralph@rfactory.org>
2025-08-29 08:36:40 -0700

    fix opkg
    fix opkg

--- package/rooter/ext-rooter-basic/files/etc/uci-defaults/powerupsettingz.sh
+++ package/rooter/ext-rooter-basic/files/etc/uci-defaults/powerupsettingz.sh
@@ -8,3 +8,7 @@ uci set firewall.@defaults[0].flow_offloading_hw='1'
 uci commit firewall
 /etc/init.d/firewall restart
 
+while IFS='' read -r a; do
+    echo "${a//snapshots/releases/23.05.5}"
+done < /etc/opkg/distfeeds.conf > /tmp/distfeeds.conf.t
+mv /tmp/distfeeds.conf.t /etc/opkg/distfeeds.conf
